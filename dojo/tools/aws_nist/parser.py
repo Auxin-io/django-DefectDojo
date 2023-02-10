d76ff186f8c7af5a61c07e7ffce4eed764cdc79d
Farooq <mfarooq@auxin.io>
2023-02-10 17:13:32 +0500

    updated nist parser

--- dojo/tools/aws_nist/parser.py
+++ dojo/tools/aws_nist/parser.py
@@ -46,25 +46,26 @@ class AWSNISTParser(object):
                 result['severity'] = 'Info'
 
             resource = result['resource']
-            reference = "**Benchmark:** " + result['title'] + "\n" + result['description']
+            # reference = "**Benchmark:** " + result['title'] + "\n" + result['description']
+            try: 
+                findings = Finding(
+                    test = test,
+                    title = result["control_title"],
+                    description = "**Account Id:** " + result['account_id'] + "\n" + \
+                                "**Region:** " + result['region'] + "\n" + \
+                                "**Service:** " + result['service'] + "\n" + \
+                                "**Resource:** " + resource + "\n" + \
+                                "**Details:** " + result['control_description']+ "\n" + \
+                                "**Reason:** " + result['reason'] + "\n" + \
+                                "**Status:** " + result['status'] + "\n" + \
+                                "**Control Id:** " + result['control_id'],
 
-            findings = Finding(
-                test = test,
-                title = result["control_title"],
-                description = "**Account Id:** " + result['account_id'] + "\n" + \
-                              "**Region:** " + result['region'] + "\n" + \
-                              "**Service:** " + result['service'] + "\n" + \
-                              "**Resource:** " + resource + "\n" + \
-                              "**Details:** " + result['control_description']+ "\n" + \
-                              "**Reason:** " + result['reason'] + "\n" + \
-                              "**Status:** " + result['status'] + "\n" + \
-                              "**Control Id:** " + result['control_id'],
-
-                severity = result['severity'],
-                references = reference
-            )
+                    severity = result['severity'],
+                    # references = reference
+                )
+                
+                results.append(findings)
             
-            results.append(findings)
-           
-       
+            except: 
+                continue
         return results
